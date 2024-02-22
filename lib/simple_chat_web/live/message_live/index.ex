43994f30e0691547839f76e20b0c5415487b025a
Feintt <0253214@up.edu.mx>
2024-02-22 14:37:06 -0600

    fixed bugs with  the messaging

--- lib/simple_chat_web/live/message_live/index.ex
+++ lib/simple_chat_web/live/message_live/index.ex
@@ -2,6 +2,7 @@ defmodule SimpleChatWeb.MessageLive.Index do
   use SimpleChatWeb, :live_view
 
   alias SimpleChat.Chat
+  alias SimpleChat.Accounts
 
   @impl true
   def mount(_params, _session, socket) do
@@ -9,25 +10,27 @@ defmodule SimpleChatWeb.MessageLive.Index do
       SimpleChatWeb.Endpoint.subscribe("chat")
     end
 
-    {:ok, assign(socket, username: username(socket), messages: [])}
+    messages = Chat.list_messages()
+    content_list = Enum.map(messages, & &1.content)
+    {:ok, assign(socket, username: username(socket), messages: content_list)}
   end
 
   @impl true
   def handle_info(%{event: "message", payload: message}, socket) do
-    {:noreply, assign(socket, messages: socket.assigns.messages ++ [message])}
+    IO.inspect("Here----------------")
+    {:noreply, assign(socket, messages: socket.assigns.messages ++ [message.text])}
   end
 
   @impl true
   def handle_event("send", %{"text" => text}, socket) do
     SimpleChatWeb.Endpoint.broadcast("chat", "message", %{
-      text: text,
-      name: socket.assigns.username
+      text: text
     })
 
     {:noreply, socket}
   end
 
   defp username(socket) do
-    socket.assigns.current_user.id
+    Accounts.get_user!(socket.assigns.current_user.id).email
   end
 end
